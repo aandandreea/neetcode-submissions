077d195047bd502e012069a709c37552c65758f5
Andreea <126575169+aandandreea@users.noreply.github.com>
2026-05-25 17:59:34 +0300

    Add: merge-two-sorted-linked-lists - submission-0

---
+++ Data Structures & Algorithms/merge-two-sorted-linked-lists/submission-0.java
@@ -0,0 +1,60 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode head = null;
+        ListNode list3curr = null;
+
+        while(list1 != null && list2 != null){
+            if(list1.val < list2.val){
+                if(head == null){
+                    head = new ListNode(list1.val);
+                    list3curr = head;
+                } else {
+                    list3curr.next = new ListNode(list1.val);
+                    list3curr = list3curr.next;
+                }
+                list1 = list1.next;
+            } else {
+                if(head == null){
+                    head = new ListNode(list2.val);
+                    list3curr = head;
+                } else {
+                    list3curr.next = new ListNode(list2.val);
+                    list3curr = list3curr.next;
+                }
+                list2 = list2.next;
+            }
+        }
+        while(list1 != null){
+            if(head == null){
+                head = new ListNode(list1.val);
+                list3curr = head;
+            } else {
+                list3curr.next = new ListNode(list1.val);
+                list3curr = list3curr.next;
+            } 
+            list1 = list1.next;
+        }
+        while(list2 != null){
+            if(head == null){
+                head = new ListNode(list2.val);
+                list3curr = head;
+            } else {
+                list3curr.next = new ListNode(list2.val);
+                list3curr = list3curr.next;
+            }
+            list2 = list2.next;
+        }
+        return head;
+    }
+}
